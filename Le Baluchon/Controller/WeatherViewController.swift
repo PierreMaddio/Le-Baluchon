f33bb4ce1c406975aaf5536955171c5480cfc401
PierreMaddio <pmsymf@gmail.com>
2022-06-14 19:03:25 +0200

    weather: add function fetchDataCity

--- Le Baluchon/Controller/WeatherViewController.swift
+++ Le Baluchon/Controller/WeatherViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
     
@@ -19,25 +20,61 @@ class WeatherViewController: UIViewController {
     
     private let weather = WeatherService()
     
+    var viewWillAppearForFirstTime: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    @IBAction func tappedWeatherButton(_ sender: UIButton) {
-        weather.getWeather(lat: 40.7127281, lon: -74.0060152) { data in
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if viewWillAppearForFirstTime {
+            fetchDataNewYork()
+            fetchDataParis()
+            viewWillAppearForFirstTime = false
+        }
+    }
+    
+    private func fetchDataNewYork() {
+        let coordinateNewYork = CLLocationCoordinate2D(latitude: 40.7127281, longitude: -74.0060152)
+        
+        fetchDataForCity(with: coordinateNewYork, for: true)
+        
+    }
+    
+    private func fetchDataParis() {
+        let coordinateParis = CLLocationCoordinate2D(latitude: 48.8588897, longitude: 2.320041)
+        fetchDataForCity(with: coordinateParis, for: false)
+        
+    }
+    
+    private func fetchDataForCity(with coordinates: CLLocationCoordinate2D, for destinationView: Bool) {
+        weather.getWeather(lat: coordinates.latitude, lon: coordinates.longitude) { data in
             if let name = data.name, let temperature = data.main?.temp {
                 DispatchQueue.main.async {
-                    self.destinationCityNameLabel.text = name
-                    self.destinationTemperatureLabel.text = String(temperature)
+                    if destinationView == true {
+                        self.destinationCityNameLabel.text = name
+                        self.destinationTemperatureLabel.text = String(temperature)
+                    } else {
+                        self.originCityNameLabel.text = name
+                        self.originTemperatureLabel.text = String(temperature)
+                    }
                 }
             }
         } failureCompletion: {
             print("failureCompletion \(#function)")
+            
         }
     }
+
     
-    func kelvinsToCelsius(temperature: Double) -> String {
+    @IBAction func tappedWeatherButton(_ sender: UIButton) {
+        fetchDataNewYork()
+        fetchDataParis()
+    }
+    
+    private func kelvinsToCelsius(temperature: Double) -> String {
         let conversion = temperature - 273.15
         let aroundConversion = conversion
         let stringConversion = String(aroundConversion)
--- Le BaluchonTests/Le_BaluchonTests.swift
+++ Le BaluchonTests/Le_BaluchonTests.swift
@@ -9,28 +9,27 @@ import XCTest
 @testable import Le_Baluchon
 
 class Le_BaluchonTests: XCTestCase {
+    
+    let maxDouble: Double = 134232332332
+    let minDouble: Double = -134232332332
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testKelvinToCelciusForMaxKelvion() throws {
+        let vc = WeatherViewController()
+
+        let result = vc.kelvinsToCelsius(temperature: maxDouble)
+        XCTAssertEqual(result, "-273.15")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testKelvinToCelciusForZeroKelvin() throws {
+        let vc = WeatherViewController()
+        let result = vc.kelvinsToCelsius(temperature: 0 )
+        XCTAssertEqual(result, "-273.15")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testKelvinToCelciusForMinKelvin() throws {
+        let vc = WeatherViewController()
+        let result = vc.kelvinsToCelsius(temperature: minDouble )
+        XCTAssertEqual(result, "-263.15")
     }
 
 }
